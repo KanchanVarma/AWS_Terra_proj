d3dcf5dbe3bb3fb8bd1c8d1004fbc8f5c9a8d76b
Kanchan Varma <57659073+KanchanVarma@users.noreply.github.com>
2024-05-16 16:53:48 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -10,10 +10,10 @@ resource "aws_instance" "web"{
 	tags = {
 		Name = "Web Server"
 	}
-	
+
 	provisioner "file" {
 		source = "./aws-login.pem"
-		destination = "home/ec2-user/aws-login.pem"
+		destination = "/home/ec2-user/aws-login.pem"
 		
 		connection {
 			type = "ssh"
@@ -27,7 +27,7 @@ resource "aws_instance" "web"{
 resource "aws_instance" "db"{
 	ami 			= "ami-08df646e18b182346" 
 	instance_type	= "t2.micro"
-	key_name 		= "aws_login"
+	key_name 		= "aws-login"
 	subnet_id 		= aws_subnet.private.id
 	vpc_security_group_ids = [aws_security_group.allow_tls_db.id]
 	
